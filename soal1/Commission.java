2acb75391b946ead30a4d9b738873057aa1c84c5
MuhammadAhdiyatul <muhammad.ahdiyatul.tif425@polban.ac.id>
2026-03-04 14:05:52 +0700

    mengubah code soal1

---
+++ soal1/Commission.java
@@ -0,0 +1,12 @@
+package soal1;
+
+public class Commission extends Hourly{
+    private double totalSales;
+    private double commissionRate;
+
+    public Commission(String name, String id, double hourlyRate, double totalSales, double commissionRate) {
+        super(eName, eAddress, ePhone, socsecNumber, rate);
+        this.totalSales = 0.0;
+        this.commissionRate = commissionRate;
+    }   
+}
