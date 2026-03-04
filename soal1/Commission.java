34762da29b61f95e994ab7723ba88460f8c19fa4
MuhammadAhdiyatul <muhammad.ahdiyatul.tif425@polban.ac.id>
2026-03-04 14:59:20 +0700

    memperbarui code

--- soal1/Commission.java
+++ soal1/Commission.java
@@ -1,12 +1,36 @@
 package soal1;
 
-public class Commission extends Hourly{
+public class Commission extends Hourly {
     private double totalSales;
     private double commissionRate;
 
-    public Commission(String name, String id, double hourlyRate, double totalSales, double commissionRate) {
-        super(eName, eAddress, ePhone, socsecNumber, rate);
-        this.totalSales = 0.0;
+    public Commission(String eName, String eAddress, String ePhone, String socSecNumber, double rate, double commissionRate) {
+        super(eName, eAddress, ePhone, socSecNumber, rate);
         this.commissionRate = commissionRate;
-    }   
+        this.totalSales = 0.0;
+    }
+
+    // Method untuk menambahkan total penjualan [cite: 16]
+    public void addSales(double totalSales) {
+        this.totalSales += totalSales;
+    }
+    
+    @Override
+    public double pay() {
+       
+        double payment = super.pay() + (totalSales * commissionRate);
+        
+        
+        totalSales = 0.0;
+        
+        return payment;
+    }
+
+    @Override
+    public String toString() {
+        // Memanggil method toString() parent, lalu menambahkan total penjualan [cite: 19]
+        String result = super.toString();
+        result += "\nTotal Sales: " + totalSales;
+        return result;
+    }
 }
--- soal1/employee.java
+++ soal1/employee.java
@@ -1,33 +1,22 @@
 package soal1;
 
-public class Employee extends StaffMember
-{
-protected String socialSecurityNumber;
-protected double payRate;
-//-----------------------------------------------------------------
-// Sets up an employee with the specified information.
-//-----------------------------------------------------------------
-public Employee (String eName, String eAddress, String ePhone,
-String socSecNumber, double rate)
-{
-super (eName, eAddress, ePhone);
-socialSecurityNumber = socSecNumber;
-payRate = rate;
-}
-//-----------------------------------------------------------------
-// Returns information about an employee as a string.
-//-----------------------------------------------------------------
-public String toString()
-{
-String result = super.toString();
-result += "\nSocial Security Number: " + socialSecurityNumber;
-return result;
-}
-//-----------------------------------------------------------------
-// Returns the pay rate for this employee.
-//-----------------------------------------------------------------
-public double pay()
-{
-return payRate;
-}
+public class Employee extends StaffMember {
+    protected String socialSecurityNumber;
+    protected double payRate;
+
+    public Employee(String eName, String eAddress, String ePhone, String socSecNumber, double rate) {
+        super(eName, eAddress, ePhone);
+        this.socialSecurityNumber = socSecNumber;
+        this.payRate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nSocial Security Number: " + socialSecurityNumber;
+    }
+
+    @Override
+    public double pay() {
+        return payRate;
+    }
 }
